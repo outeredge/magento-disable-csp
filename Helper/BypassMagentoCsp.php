3fd108c7430c1ae4ceb92a359854f627e1d4306a
Bruno Dias <ibroopi@gmail.com>
2024-05-29 12:06:57 +0100

    Improve bits

--- Helper/BypassMagentoCsp.php
+++ Helper/BypassMagentoCsp.php
@@ -2,23 +2,10 @@
 
 namespace OuterEdge\DisableCsp\Helper;
 
-use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Module\Manager;
-use Magento\Csp\Helper\CspNonceProvider;
-
 class BypassMagentoCsp
 {
-    public function __construct(
-        protected Manager $moduleManager
-    ) {
-    }
-
     public function generateNonce(): string
     {
-        if ($this->moduleManager->isOutputEnabled('Magento_Csp')) {
-            $cspNonceProvider = ObjectManager::getInstance()->get(CspNonceProvider::class);
-            return $cspNonceProvider->generateNonce();
-        }
         return '';
     }
 }
